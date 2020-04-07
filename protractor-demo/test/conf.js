8e72fd2eb9128f66b90d858d21d6b54b212ed0ee
cjshare29 <cjshare29@gmail.com>
2020-04-07 13:12:02 +0100

    running parallel tests running headless tests

--- protractor-demo/test/conf.js
+++ protractor-demo/test/conf.js
@@ -1,14 +1,36 @@
 // Tests for the calculator.
 exports.config = {
-  directConnect: true,
+//  directConnect: false,
+  'seleniumAddress': 'http://192.168.1.47:4444/wd/hub',
 
   framework: 'jasmine2',
 
   specs: [
-    '${bamboo_inject_var1}'
+    'spec.js',
+    'spec2.js'
   ],
 
-  capabilities: {
-    'browserName': 'chrome'
-  },
+    capabilities: {
+
+      browserName: 'chrome',
+//      var opt = new chromeOptions(),
+//      ChromeDriver driver = new ChromeDriver("/Downloads/chromedriver-3");
+
+      'goog:chromeOptions':{
+      args: ["--headless", "--disable-gpu", "--window-size=800x600"]
+      },
+      shardTestFiles: 2,
+
+      maxInstances: '2',
+    },
+
+
+//  multiCapabilities: [
+//    {
+//    browserName: 'chrome',
+//    },{
+//    browserNa/me: 'firefox',
+//    },
+////    maxInstances: '2'
+//  ],
 };
